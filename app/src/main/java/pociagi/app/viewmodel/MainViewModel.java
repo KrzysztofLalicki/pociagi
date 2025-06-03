43592ffd9c19c2c404a665b3b479fecc932a8bb5
qbad3000 <domanskiquba@gmail.com>
2025-06-03 05:34:04 +0200

    add tab view

---
+++ app/src/main/java/pociagi/app/viewmodel/MainViewModel.java
@@ -0,0 +1,25 @@
+package pociagi.app.viewmodel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class MainViewModel {
+    @FXML private TabPane tabPane;
+    @FXML private Tab rozkladJazdy;
+    @FXML private Tab wyszukiwarkaPolaczen;
+
+    @FXML
+    public void initialize() {
+//        tabPane.getSelectionModel().selectedItemProperty().addListener((obs, oldTab, newTab) -> {
+//            if (newTab != null) {
+//                System.out.println("Selected tab: " + newTab.getText());
+//                if (newTab == rozkladJazdy) {
+//                    onRozkladJazdySelected();
+//                } else {
+//                    onOtherTabSelected();
+//                }
+//            }
+//        });
+    }
+}
